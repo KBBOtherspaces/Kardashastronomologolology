2a533cf56b5c755a4261535ea3b3565e5063597c
KBBOtherspaces <kristinbauerart@gmail.com>
2025-10-15 21:34:55 -0700

    Save current changes before merge

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ const spaceContainerISS = document.getElementById("craft-iss");
 const spaceContainerTian = document.getElementById("craft-tiangong");
 const allCrafts = document.querySelectorAll(".craft");
 
-function checkForAstronauts(button) {
+function consult the stars(button) {
   button.style.display = "none"; // hide button
 
   //   The line below will pull the data from the API
   fetch(
-    "https://corquaid.github.io/international-space-station-APIs/JSON/people-in-space.json"
+    "astros.json"
   )
     //   The line below will pull the data from the locally stored JSON file
     //   fetch("/ex-json/astros.json")
@@ -20,7 +20,7 @@ function checkForAstronauts(button) {
       // Update the Message
       document.getElementById(
         "status"
-      ).innerHTML = `Whoa there are ${data.number} people in space rn!!`;
+      ).innerHTML = `Whoa there are ${data.number} people out in orbit!!`;
 
       // Add each Astronaut to their craft
       var astros = data.people;
